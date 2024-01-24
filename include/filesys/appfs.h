06fd3d2005359a0b6448366ebd8220087b486a7d
Julian Scheffers <julian@scheffers.net>
2024-01-24 13:10:33 +0100

    a

---
+++ include/filesys/appfs.h
@@ -0,0 +1,14 @@
+
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <stdint.h>
+
+// To bootloader data.
+typedef struct {
+    uint64_t appfs_magic;
+    uint8_t  app;
+    uint8_t  padding[7];
+    char     arg[64];
+} tobootloader_t;
